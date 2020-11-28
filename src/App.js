7b0a47f08cace6356726eb440fb6fe3c7f561a89
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-11-28 15:31:20 -0500

    useRef added
    To close dropdown menu through a sophisticated use of event bubbling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './Components/Accordion';
 import Search from './Components/Search';
+import Dropdown from './Components/Dropdown';
 
 const items = [
   {
@@ -17,11 +18,32 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
