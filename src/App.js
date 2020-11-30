e00df8368d0b633798557c538ae4e3a756fc4814
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-11-30 15:44:18 -0500

    Routing from scratch
    A Route component was made to create routes without using React Router.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './Components/Accordion';
 import Search from './Components/Search';
 import Dropdown from './Components/Dropdown';
 import Translate from './Components/Translate';
+import Route from './Components/Route';
 
 const items = [
   {
@@ -34,26 +35,52 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+};
+
 export default () => {
   const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowdropdown] = useState(true);
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <button onClick={() => setShowdropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
         <Dropdown
           label="Select a Color"
-          text="Changing Text Color"
+          options={options}
           selected={selected}
           onSelectedChange={setSelected}
-          options={options}
+          text="Changing the Color"
         />
-      ) : null} */}
-      <Translate />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
